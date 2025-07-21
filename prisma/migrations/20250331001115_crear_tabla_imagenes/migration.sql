069f8dd6fc526d94030ac44083f812912eb2651a
Cristian Villalobos <cristianvillalobosvv@gmail.com>
2025-07-21 00:18:13 -0400

    Limpieza

--- prisma/migrations/20250331001115_crear_tabla_imagenes/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Imagen" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "url" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "descripcion" TEXT,
-    "fechaSubida" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
